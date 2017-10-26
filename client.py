ed7cc61fbfbaaff8dd00847235ca3886c20d1f20
miguelangel3 <miguelangel.abb@gmail.com>
2017-10-26 15:35:20 +0200

    Modifico las variables que regogen los atributos de la shell para simplificar el código además añado el control de error del apartado 4.7

--- client.py
+++ client.py
@@ -13,14 +13,15 @@ import sys
 #LINE = '¡Hola mundo!'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-def comunication (server,port,line):
+def comunication (server,port,sip_type,name,expires_value):
 
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-        my_socket.connect((SERVER, PORT))
-        print("Enviando:", line)
-        info = line.split(" ")
-        msg_to_send = "REGISTER " + "sip:" + info[1] +" SIP/2.0"
-        my_socket.send(bytes(msg_to_send, 'utf-8') + b'\r\n\r\n')
+        my_socket.connect((server, port))
+        #print("Enviando:", line)
+        #info = line.split(" ")
+        msg_to_send = "REGISTER " + "sip:" + name +" SIP/2.0" + "\r\n"
+        msg_to_send = msg_to_send + "Expires:" + expires_value + "\r\n"
+        my_socket.send(bytes(msg_to_send, 'utf-8') + b'\r\n')
         data = my_socket.recv(1024)
         print('Recibido -- ', data.decode('utf-8'))
 
@@ -29,12 +30,18 @@ def comunication (server,port,line):
 
 if __name__ == '__main__':
 
+    if len(sys.argv) != 6:
+        sys.exit("Usage: client.py ip puerto register sip_address " + \
+        "expires_value")
     try:
-        SERVER = sys.argv[1]
-        PORT = int(sys.argv[2])
-        line = " ".join(sys.argv[3:])
+        server = sys.argv[1]
+        port = int(sys.argv[2])
+        sip_type = sys.argv[3]
+        name = sys.argv[4]
+        expires_value = sys.argv[5]
+        #line = " ".join(sys.argv[3:])
 
     except (IndexError):
         sys.exit("Usage: server,port,line")
 
-    comunication(SERVER,PORT,line)
+    comunication(server,port,sip_type,name,expires_value)
