2383b339f58bc8f18d16c013048066b333412540
miguelangel3 <miguelangel.abb@gmail.com>
2017-10-28 17:54:09 +0200

    limpio código

--- client.py
+++ client.py
@@ -7,18 +7,14 @@ Programa cliente UDP que abre un socket a un servidor
 import socket
 import sys
 
-# Constantes. Dirección IP del servidor y contenido a enviar
-#SERVER = 'localhost'
-#PORT = 6001
-#LINE = '¡Hola mundo!'
-
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 def comunication (server,port,sip_type,name,expires_value):
+    """
+    Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+    """
+
 
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
         my_socket.connect((server, port))
-        #print("Enviando:", line)
-        #info = line.split(" ")
         msg_to_send = "REGISTER " + "sip:" + name +" SIP/2.0" + "\r\n"
         msg_to_send = msg_to_send + " Expires: " + expires_value + "\r\n"
         my_socket.send(bytes(msg_to_send, 'utf-8') + b'\r\n')
@@ -39,7 +35,6 @@ if __name__ == '__main__':
         sip_type = sys.argv[3]
         name = sys.argv[4]
         expires_value = sys.argv[5]
-        #line = " ".join(sys.argv[3:])
 
     except (IndexError):
         sys.exit("Usage: server,port,line")
